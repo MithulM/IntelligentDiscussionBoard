3107a5396604a97a28406cac9b79cf634e2dd8d9
Mithul Manivannan <mithulmanimala@gmail.ccom>
2023-03-25 16:57:22 -0500

    Added nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import { useState } from 'react'
 import HomePage from './HomePage.jsx'
-import './HomePage.css'
+import DummyClass from './DummyClass.jsx'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Router>
+        <div className="menu">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/class1">Class 1</Link>
+            </li>
+            <li>
+              <Link to="/class2">Class 2</Link>
+            </li>
+          </ul>
+        </div>
+        <Routes className="main">
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/class1" element={<DummyClass/>}/>
+        </Routes>
+      </Router >
     </div>
   )
 }
--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,23 +1,14 @@
-import { useState } from 'react';
-import './App.css'
+import React, { useState } from 'react';
 import './HomePage.css'
 
+
 function HomePage() {
+    const [content, setContent] = useState('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consequat aliquam nisi, in venenatis dolor bibendum vel. Sed ac purus dignissim, placerat magna ac, malesuada tellus. Sed vel sodales orci, sed interdum ipsum. Vivamus eu tortor nisl. Suspendisse ultricies nibh nec tortor laoreet rutrum. Integer ac massa nulla. Nullam convallis malesuada est, sed cursus magna tincidunt ac.');
+
     return (
-        <div className="App">
-            <div className="menu">
-                <ul>
-                    <li>Menu item 1</li>
-                    <li>Menu item 2</li>
-                    <li>Menu item 3</li>
-                </ul>
-            </div>
-            <div className="main">
-                <div className="content">
-                    {/* Main content goes here */}
-                </div>
-            </div>
-        </div>
+        <React.Fragment>
+            <div className="content">{content}</div>
+        </React.Fragment>
     );
 }
 
