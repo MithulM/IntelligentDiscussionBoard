d2a86e67dfc11d11afbe72ad3242a871be140ecb
Mithul Manivannan <mithulmanimala@gmail.ccom>
2023-03-30 16:37:42 -0500

    Added Home page posts and pos with them

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -3,9 +3,9 @@ import './HomePage.css'
 
 function HomePage() {
 
-    const randCont = () => {
-        const length = Math.floor(Math.random() * 501) + 500;
-        const characters = 'abcdefghijklmnopqrstuvwxyz       ';
+    const randCont = (min, max) => {
+        const length = Math.floor(Math.random() * (max - min + 1)) + min;
+        const characters = 'abcdefghijklmnopqrstuvwxyz      ';
         let result = '';
         for (let i = 0; i < length; i++) {
             result += characters.charAt(Math.floor(Math.random() * characters.length));
@@ -13,29 +13,37 @@ function HomePage() {
         return result;
     }
 
+    // Create an object with professer, recommended, self, recent keys, and values for each have an id, question, and answer in lorem ipsum.
+    let data = []
+    let topics = ["Professor", "Your", "Recent", "Recommended"]
+    for (let i = 0; i < topics.length; i++) {
+        let postList = []
+        for (let j = 0; j < 5; j++) {
+            postList.push({
+                id: j,
+                question: randCont(10, 20),
+                answer: randCont(200, 400)
+            })
+        }
+        data.push({
+            topic: topics[i],
+            id: i,
+            posts: postList
+        }
+        );
+    }
+
     return (
         <div className="main">
-            <div>
-                {randCont()}
-            </div>
-            <div>
-                {randCont()}
-            </div>
-            <div>
-                {randCont()}
-            </div>
-            <div>
-                {randCont()}
-            </div>
-            <div>
-                {randCont()}
-            </div>
-            <div>
-                {randCont()}
-            </div>
-            <div>
-                {randCont()}
-            </div>
+            {data.map((item) => (<div id={item.id} className="HomePageConvenience">
+                <div>
+                    <h1>{item.topic} posts</h1>
+                    {item.posts.map((item2) => (<div id={item2.id} className="post">
+                        <h2>{item2.question}?</h2>
+                        <p>{item2.answer}</p>
+                    </div>))}
+                </div>
+                </div>))}
         </div>
     );
 }
