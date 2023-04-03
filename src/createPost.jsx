575306ec6f0c47a9a4cc6d330545a2786764e43a
Marzuq <marzuqq23@gmail.com>
2023-04-03 18:52:06 -0500

    Created the Post Creation and Similar Post Page

--- src/createPost.jsx
+++ src/createPost.jsx
@@ -1,30 +1,63 @@
 import React from 'react';
-import './createPost.css'
-import './HomePage.css'
+import './createPost.css';
 
 function CreatePost() {
-    return (
-        <div>
-            <html>
-            <head>
-            <title>Title of the document</title>
-            </head>
-        <body>
-            <form action="/createpost" method="post">
-            <textarea id="text" name="text" placeholder="Title" rows="1" cols="1"></textarea>
-            <br>
-            </br>
-            <textarea id="text" name="text" placeholder="Enter text" rows="12" cols="50"></textarea>
-            <br/>
-            <li class="button">
-            <button type="submit">Post</button>
-            </li>
-            </form>
-        </body>
-            </html>
+  return (
+    <div className="create-post-container">
+      <div className="create-post"style={{ marginLeft: '40px' }}>
+        <h2 className="create-post__title">Create Post</h2>
+        <form className="create-post__form" action="/createpost" method="post">
+          <label className="create-post__label" htmlFor="post-title">
+            Post Title
+          </label>
+          <textarea
+            className="create-post__input"
+            id="post-title"
+            name="post-title"
+            placeholder="Title"
+            rows="1"
+            cols="50"
+          ></textarea>
+          <label className="create-post__label" htmlFor="post-body">
+            Post Body
+          </label>
+          <textarea
+            className="create-post__input create-post__input--body"
+            id="post-body"
+            name="post-body"
+            placeholder="Enter text"
+            rows="15"
+            cols="50"
+          ></textarea>
+          <label className="create-post__label" htmlFor="post-class">
+          </label>
+          <select
+            className="create-post__select"
+            id="post-class"
+            name="post-class"
+          >
+            <option value="">Select Class</option>
+            <option value="class1">Class 1</option>
+            <option value="class2">Class 2</option>
+          </select>
+          <button className="create-post__button" type="submit">
+            Post
+          </button>
+        </form>
       </div>
-    )
-
+      <div className="similar-posts">
+        <h2 className="similar-posts__title">Similar Posts</h2>
+        <textarea
+          className="similar-posts__input similar-posts__input--body"
+          id="similar-posts-body"
+          name="similar-posts-body"
+          placeholder="Enter text"
+          rows="41"
+          cols="100"
+        ></textarea>
+      </div>
+    </div>
+  );
 }
 
 export default CreatePost;
