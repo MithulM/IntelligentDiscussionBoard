800a568e9f0d38c39647ac18e6387d218d6b9e0b
Marzuq Quraishi <73802175+MarzuqQ@users.noreply.github.com>
2023-04-23 15:35:06 -0500

    Fixed UI for  nested comments, SinglePostPage, comments

--- src/SinglePostPage.jsx
+++ src/SinglePostPage.jsx
@@ -23,20 +23,24 @@ function SinglePostPage() {
 
     return (
         <div className="single-post-page">
-            <div className="post-body-container">
-                <div className="onePostInfo">
-                    <span className="post-course-title">{post.course && `Course: ${post.course.course_number}`}&nbsp;</span>
-                    <span className="post-date">Posted: {post.time_created && `${getTimeAgoString(post.time_created)}`}</span>
+            <div className= "wrap-everything">
+            <div className="post-container">
+                <div className="post-body-container">
+                    <div className="onePostInfo">
+                        <span className="post-course-title">{post.course && `Course: ${post.course.course_number}`}&nbsp;</span>
+                        <span className="post-date">Posted: {post.time_created && `${getTimeAgoString(post.time_created)}`}</span>
+                    </div>
+                    <h2 className="post-body-title">{post.post_title}</h2>
+                    <div className="post-body-text">{post.post_content}</div>
+                    <br></br>
+                    <PostFooter />
                 </div>
-                <h2 className="post-body-title">{post.post_title}</h2>
-                <div className="post-body-text">{post.post_content}</div>
-                <br></br>
-                <PostFooter />
             </div>
-            <div className="replies">
+            <div className="comment-container">
                 {comments && <span className="section-title">Replies:</span>}
                 <Comment depth={0} comments={comments} />
             </div>
+        </div>
         </div>
     );
 }
